aec227aafb347989f171a88b33e2ccb1921bfed0
Subharthy Kuiry <subharthykuiry@gmail.com>
2026-02-03 19:25:55 +0530

    Fixed properties and enabled debug logs

--- mongodbexample/src/main/java/mongodbexample/MongodbexampleApplication.java
+++ mongodbexample/src/main/java/mongodbexample/MongodbexampleApplication.java
@@ -1,17 +1,3 @@
-// package mongodbexample;
-
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-// @SpringBootApplication
-// public class MongodbexampleApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(MongodbexampleApplication.class, args);
-// 	}
-
-// }
-
 package mongodbexample;
 
 import org.springframework.boot.SpringApplication;
@@ -19,12 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MongodbexampleApplication {
-
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(MongodbexampleApplication.class, args);
-        } catch (Exception e) {
-            e.printStackTrace(); // This forces the hidden error to show up!
-        }
+        SpringApplication.run(MongodbexampleApplication.class, args);
     }
 }
